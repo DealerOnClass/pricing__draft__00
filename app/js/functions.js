da13109fec96dcd54e734e0f289f6e42d75a037a
DealerOnClass <alberto@dealeron.com>
2015-12-02 11:49:27 -0500

    here we go testing

--- app/js/functions.js
+++ app/js/functions.js
@@ -25,36 +25,42 @@ $("#navbar-main .navbar-sidebar-wrapper a[href^='#ui__']").on('click', function(
         window.location.hash = hash;
     });
 });
-$("#toTop").on('click', function(e) {
-    e.preventDefault();
-    var hash = this.hash;
-    $('html, body').stop().animate({
-        scrollTop: $(hash).offset().top
-    }, 1500, 'easeInOutExpo', function() {
-        window.location.hash = hash;
-    });
-});
+//-------------------------------------
+//      $("#toTop").on('click', function(e) {
+//          e.preventDefault();
+//          var hash = this.hash;
+//          $('html, body').stop().animate({
+//              scrollTop: $(hash).offset().top
+//          }, 1500, 'easeInOutExpo', function() {
+//              window.location.hash = hash;
+//          });
+//      });
+//-------------------------------------
 
 //
 //
 //  Thank you Cody House ** start
 //  https://codyhouse.co/demo/back-to-top/index.html
 // browser window scroll (in pixels) after which the "back to top" link is shown
-var offset = 300,
-    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-    offset_opacity = 1200,
-    //duration of the top scrolling animation (in ms)
-    scroll_top_duration = 700,
-    //grab the "back to top" link
-    $back_to_top = $('#toTop');
+//-------------------------------------
+//      var offset = 300,
+//          //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+//          offset_opacity = 1200,
+//          //duration of the top scrolling animation (in ms)
+//          scroll_top_duration = 700,
+//          //grab the "back to top" link
+//          $back_to_top = $('#toTop');
+//-------------------------------------
 
 //hide or show the "back to top" link
-$(window).scroll(function(){
-    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('in') : $back_to_top.removeClass('in');
-    if( $(this).scrollTop() > offset_opacity ) {
-        $back_to_top.addClass('fade');
-    }
-});
+//-------------------------------------
+//      $(window).scroll(function(){
+//          ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('in') : $back_to_top.removeClass('in');
+//          if( $(this).scrollTop() > offset_opacity ) {
+//              $back_to_top.addClass('fade');
+//          }
+//      });
+//-------------------------------------
 //  Thank you Cody House ** end
 
 
@@ -92,9 +98,9 @@ $(".panel-wizard").on("hide.bs.collapse", function() {
 //
 //  Open Nav Bar
 //
-$('[data-animate="icon-bar"]').click(function(){
-    $(this).toggleClass('navbar-sidebar-is-open');
-});
+//  $('[data-animate="icon-bar"]').click(function(){
+//      $(this).toggleClass('navbar-sidebar-is-open');
+//  });
 //  //  Set icon default && active state if open
 //  $(document).ready( function() {
 //      var icon = $(".navbar-sidebar-collapse.in").attr("data-animate-icon");
