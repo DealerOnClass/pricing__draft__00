dddb2b646e209897381379e74a7c39b95dd05a6e
DealerOnClass <alberto@dealeron.com>
2016-01-07 12:41:53 -0500

    mhmm

--- app/js/functions.js
+++ app/js/functions.js
@@ -93,7 +93,6 @@ $(document).ready( function() {
 });
 $(window).resize( function() {
     UpdateDimensions();
-    //  UpdateSidebar();
 });
 function UpdateDimensions() {
     var navHeight        = $('[oncanvas-nav]').height();
